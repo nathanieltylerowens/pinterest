79fac3d4192b1f20174fd99eb79243ab421132a6
NateOwens <nathanieltylerowens@gmail.com>
2020-06-28 20:00:49 -0500

    added auth and logout

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import utils from '../../helpers/utils';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
+
+const loginButton = () => {
+  const domstring = '<button id="google-auth" class="d-inline btn btn-outline-danger"><i class="fab fa-google"></i>Login</button>';
+
+  utils.printToDom('#auth', domstring);
+
+  $('#google-auth').click(signMeIn);
+};
+
+export default { loginButton };
